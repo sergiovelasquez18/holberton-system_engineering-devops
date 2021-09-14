ac086cc2cc78c1b9c04e964541b7498f41b4d4b1
sergiovelasquez18 <2458@holbertonschool.com>
2021-09-13 22:04:01 -0500

    data from an api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+script that, using this REST API, for a given employee ID,
+returns information
+'''
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    R = requests.get('https://jsonplaceholder.typicode.com/users/{:}'
+                     .format(argv[1])).json()
+    R_two = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/?userId={:}'
+        .format(argv[1])).json()
+
+    EMPLOYEE_NAME = R.get('name')
+    TASK_TITLE = []
+    NUMBER_OF_DONE_TASKS = 0
+
+    for task in R_two:
+        if task.get('completed') is True:
+            TASK_TITLE.append(task.get('title'))
+            NUMBER_OF_DONE_TASKS += 1
+    TOTAL_NUMBER_OF_TASKS = len(R_two)
+    print("Employee {} is done with tasks({}/{}):".
+          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
+                 TOTAL_NUMBER_OF_TASKS))
+    for t in TASK_TITLE:
+        print("\t {}".format(t))
