80e2ff8c197ac028fc4a313640c0a2ae52739b31
sergiovelasquez18 <2458@holbertonschool.com>
2021-09-13 22:34:18 -0500

    export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+"""
+
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    uid = argv[1]
+    id_url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
+    id_user = requests.get(id_url, verify=False).json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        uid)
+    todo = requests.get(url, verify=False).json()
+    with open("{}.csv".format(uid), 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for t in todo:
+            taskwriter.writerow([int(uid), id_user.get('username'),
+                                 t.get('completed'),
+                                 t.get('title')])
