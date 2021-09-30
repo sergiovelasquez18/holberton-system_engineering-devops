acd27c89bc32ff6b868da74f1edbbb92b7fe6815
sergiovelasquez18 <2458@holbertonschool.com>
2021-09-29 21:17:11 -0500

    Fix wp-setting file extension error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fix wp-setting file extension error
+exec { 'killmenow':
+    command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+    path    => '/bin',
+}
